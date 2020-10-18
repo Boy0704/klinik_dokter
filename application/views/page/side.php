ae612f31bf6a51624e528c664f463312bc8bc79a
Boy Kurniawan <boykurniawan123@gmail.com>
2020-10-18 13:25:30 +0700

    up

--- application/views/page/side.php
+++ application/views/page/side.php
@@ -38,6 +38,8 @@
         <li><a href="pembayaran"><i class="fa fa-money"></i> <span>Transaksi Pembayaran </span></a></li>
         <li><a href="app/cetak"><i class="fa fa-print"></i> <span>Cetak Laporan </span></a></li>
         <li><a href="a_user"><i class="fa fa-users"></i> <span>Data User </span></a></li>
+        <li><a href="a_user"><i class="fa fa-database"></i> <span>Back Up Data </span></a></li>
+        <li><a href="a_user"><i class="fa fa-file"></i> <span>Restore Data </span></a></li>
 
         <?php } elseif ($this->session->userdata('level') == 'user') {?>
 
@@ -51,7 +53,7 @@
         
         
 
-        <li class="header">LABELS</li>
+        <!-- <li class="header">LABELS</li> -->
         <!-- <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Faqs</span></a></li>
         <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>About</span></a></li> -->
       </ul>
