eefcb7c5bd3f712e1725724163682841b85707cc
Boy Kurniawan <boykurniawan123@gmail.com>
2020-11-08 14:00:08 +0700

    up

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -158,7 +158,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; <?php echo date('Y') ?> <a href="https://jualkoding.com">Boy Kurniawan,S.Kom</a>.</strong> All rights
+    <strong>Copyright &copy; <?php echo date('Y') ?> <a href="#">Klinik Dokter</a>.</strong> All rights
     reserved.
   </footer>
 
